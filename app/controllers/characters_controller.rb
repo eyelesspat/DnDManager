2907f71e8f2e4504cc5b5f574547c968b58440c2
Patryk Gadziński <pgadzinski@cross-net.eu>
2024-07-16 12:39:47 +0200

    Update CRUD

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -21,6 +21,27 @@ class CharactersController < ApplicationController
     end
   end
 
+  def edit
+    @character = Character.find(params[:id])
+  end
+
+  def update
+    @character = Character.find(params[:id])
+
+    if @character.update(character_params)
+      redirect_to @character
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @character = Character.find(params[:id])
+    @character.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
   private
 
   def character_params
