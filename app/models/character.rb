511e0727cb64645fff07300f0d7341ca74d1770f
Patryk Gadziński <pgadzinski@cross-net.eu>
2024-08-01 15:40:18 +0200

    Correct character creating form

--- app/models/character.rb
+++ app/models/character.rb
@@ -4,4 +4,8 @@ class Character < ApplicationRecord
   has_one_attached :image
 
   validates :name, presence: true
+  validates :description, presence: true
+  validates :total_hitpoints, presence: true
+  validates :total_resource, presence: true
+  validates :image, presence: true
 end
